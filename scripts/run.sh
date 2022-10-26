30dfbc52964d596ea48c8b750b2ed2ec1ab48cd0
TotalLag <>
2022-10-26 18:32:44 +0000

    Apply CI/CD changes

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,23 +3,25 @@ set -e
 
 # Restore the database if it does not already exist.
 if [ -f /directus/database/database.sqlite ]; then
-	echo "Database already exists, skipping restore"
+  echo "Database already exists, skipping restore"
 else
-	echo "No database found, restoring from replica if exists"
-	litestream restore -v -parallelism 64 -if-replica-exists -o /directus/database/database.sqlite "/directus/database/database.sqlite"
+  echo "No database found, restoring from replica if exists"
+  litestream restore -v -parallelism 64 -if-replica-exists -o /directus/database/database.sqlite "/directus/database/database.sqlite"
 fi
 
 npx directus bootstrap &&
 
 if [[ -z "${SNAPSHOT_URL}" ]]; then
-  
-	if [ -f /etc/snapshot.yml ]; then
-		npx directus schema apply --yes /etc/snapshot.yml
-	fi
+
+  if [ -f /etc/snapshot.yml ]; then
+    echo "Snapshot file found on disk, attempting to apply..."
+    npx directus schema apply --yes /etc/snapshot.yml
+  fi
 
 else
 
-	curl "${SNAPSHOT_URL}"
+  echo "Snapshot URL found, attempting to download and apply..."
+  wget "${SNAPSHOT_URL}"
   npx directus schema apply --yes snapshot.yml
 
 fi
