4b4b520c54f84ef59b6b525d59180343f0f35780
TotalLag <>
2022-10-26 21:07:00 +0000

    Apply CI/CD changes

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,24 +9,20 @@ else
   litestream restore -v -parallelism 64 -if-replica-exists -o /directus/database/database.sqlite "/directus/database/database.sqlite"
 fi
 
-npx directus bootstrap
-
 if [[ -z "${SNAPSHOT_URL}" ]]; then
 
-  if [ -f /etc/snapshot.yml ]; then
-    echo "Snapshot file found on disk, attempting to apply..."
-    npx directus schema apply --yes /etc/snapshot.yml
-  fi
+    echo "No schema snapshot present."
 
 else
 
   echo "Snapshot URL found, attempting to download and apply..."
-  wget -O ./snapshot.yml "${SNAPSHOT_URL}"
-  npx directus schema apply --yes ./snapshot.yml
+  wget -O ./snapshot.yml "${SNAPSHOT_URL}" &&
+  npx directus schema apply --yes ./snapshot.yml && rm -f ./snapshot.yml &&
+  echo "Snapshot applied!"
 
 fi
 
-ls -alh .
+npx directus bootstrap &&
 
 # Run litestream with your app as the subprocess.
 exec litestream replicate -exec "npx directus start"
